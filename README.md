42de58f30528b6b42325c8cd35959d3e425abac0
FrancescoBorzi <borzifrancesco@gmail.com>
2020-12-19 02:49:34 +0100

    feat: portals in every capital

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # portals-in-all-capitals
 SQL script to add portals in every capital
+
+Use `portals-in-all-capitals.up.sql` to apply the changes. 
+If you'd like to revert them, use `portals-in-all-capitals.down.sql`.
+
+Always check the SQL scripts before executing them in order to avoid ID conflicts. 
---
+++ portals-in-all-capitals.down.sql
@@ -0,0 +1,35 @@
+-- !!! NOTE: check these before running the queries in order to avoid conflicts !!!
+SET @GO_TEMPLATE = 500000;
+
+DELETE FROM `gameobject_template` WHERE `entry` IN (
+    @GO_TEMPLATE + 0,
+    @GO_TEMPLATE + 1,
+    @GO_TEMPLATE + 2,
+    @GO_TEMPLATE + 3,
+    @GO_TEMPLATE + 4,
+    @GO_TEMPLATE + 5,
+    @GO_TEMPLATE + 6,
+    @GO_TEMPLATE + 7
+);
+
+DELETE FROM `gameobject_template_addon` WHERE `entry` IN (
+  @GO_TEMPLATE + 0,
+  @GO_TEMPLATE + 1,
+  @GO_TEMPLATE + 2,
+  @GO_TEMPLATE + 3,
+  @GO_TEMPLATE + 4,
+  @GO_TEMPLATE + 5,
+  @GO_TEMPLATE + 6,
+  @GO_TEMPLATE + 7
+);
+
+DELETE FROM `gameobject` WHERE `id` IN (
+    @GO_TEMPLATE + 0,
+    @GO_TEMPLATE + 1,
+    @GO_TEMPLATE + 2,
+    @GO_TEMPLATE + 3,
+    @GO_TEMPLATE + 4,
+    @GO_TEMPLATE + 5,
+    @GO_TEMPLATE + 6,
+    @GO_TEMPLATE + 7
+);
